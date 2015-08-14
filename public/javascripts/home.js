190e11d84cb63ecca7bac708ab969abe29e865b5
kailevy <kai0levy@gmail.com>
2015-08-14 16:31:22 -0700

    Update to check api on load

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,6 +1,6 @@
 var app = angular.module('radar', []);
 
-app.controller('mainCtrl', function($scope, $log, env_factory, factory) {
+app.controller('mainCtrl', function ($scope, $log, env_factory, factory) {
   $scope.message = {'type':'info'};
   $scope.repo = 'shippable/support';
   $scope.accessToken = '';
@@ -11,9 +11,11 @@ app.controller('mainCtrl', function($scope, $log, env_factory, factory) {
   $scope.loading = false;
   $scope.log = $log;
   $scope.env = {};
+  $scope.apiVersion = '';
+  $scope.apiBuildnum = '';
 
   env_factory.get()
-  .success(function(data) {
+  .success(function (data) {
     if (data.NODE_ENV)
       $scope.env.environment = data.NODE_ENV;
     if (data.API_URL)
@@ -24,20 +26,28 @@ app.controller('mainCtrl', function($scope, $log, env_factory, factory) {
       $scope.env.api_url = "http://localhost:3001";
     if ($scope.env.environment == 'dev')
       $log.info($scope.env);
+    factory.info($scope.env.api_url)
+    .success(function (data) {
+      $scope.apiVersion = data.version;
+      $scope.apiBuildnum = data.buildnumber;
+    })
+    .error(function (reason) {
+      $scope.message = {'type': 'error', 'text': 'Unable to connect to API!'};
+    });
   })
-  .error(function(reason) {
+  .error(function (reason) {
     $log.error(reason);
   });
 
-  $scope.getIssues = function(state) {
-    if(checkValid()){
+  $scope.getIssues = function (state) {
+    if (checkValid()) {
       $scope.loading = true;
       factory.get($scope.env.api_url,$scope.repo,$scope.accessToken,$scope.days,$scope.daysEnd,state)
-      .success(function(data) {
+      .success(function (data) {
         checkState(data.state);
         $scope.indexData = data.indexData;
       })
-      .error(function(reason) {
+      .error(function (reason) {
         $scope.loading = false;
         $log.error(reason);
         $log.error('Trying to access ' + $scope.env.api_url);
@@ -46,7 +56,7 @@ app.controller('mainCtrl', function($scope, $log, env_factory, factory) {
     }
   };
 
-  $scope.back = function() {
+  $scope.back = function () {
     $scope.state = 'submit';
     $scope.indexData = {};
     $scope.message = {};
@@ -54,15 +64,15 @@ app.controller('mainCtrl', function($scope, $log, env_factory, factory) {
   };
 
   function checkState(state) {
-    if(state == 'open' || state == 'closed'){
+    if (state == 'open' || state == 'closed') {
       $scope.state = state;
     }
     else{
-      if (state == 'accessError'){
+      if (state == 'accessError') {
         $log.error('Access error');
         $scope.message = {'type':'error','text':'Please check your access token!'};
       }
-      else if (state == 'repoError'){
+      else if (state == 'repoError') {
         $log.error('Repo error');
         $scope.message = {'type':'error','text':'Invalid repo!'};
       }
@@ -80,10 +90,10 @@ app.controller('mainCtrl', function($scope, $log, env_factory, factory) {
       $scope.message = {'type':'error','text':'Please give your access token!'};
       return false;
     }
-    else if(isNaN(parseInt($scope.days)) || isNaN(parseInt($scope.daysEnd)) ||
+    else if (isNaN(parseInt($scope.days)) || isNaN(parseInt($scope.daysEnd)) ||
       $scope.daysEnd - $scope.days < 0 ||
       $scope.days < 0 || $scope.daysEnd <  0||
-      $scope.days % 1 !== 0 || $scope.daysEnd % 1 !== 0){
+      $scope.days % 1 !== 0 || $scope.daysEnd % 1 !== 0) {
         $scope.message = {'type':'error','text':'Please put a valid range of days!'};
         return false;
     }
@@ -93,22 +103,26 @@ app.controller('mainCtrl', function($scope, $log, env_factory, factory) {
   }
 });
 
-app.factory('factory', function($http){
+app.factory('factory', function ($http) {
   return {
-    get: function(url,repo,token,days,daysEnd,state) {
-      return $http.get(url + 
+    get: function (url, repo, token, days, daysEnd, state) {
+      return $http.get(url +
         '/issues?&repo=' + repo +
         '&token=' + token +
         '&days=' + days +
         '&daysEnd=' + daysEnd +
         '&state=' + state);
+    },
+    info: function (url) {
+      console.log(url);
+      return $http.get(url + '/info');
     }
   };
 });
 
-app.factory('env_factory', function($http){
+app.factory('env_factory', function ($http) {
   return {
-    get: function() {
+    get: function () {
       return $http.get('/env');
     }
   };
